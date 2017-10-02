28973a7369f727c74d2e50dc08e26707b8f05866
Apple <apple@Macbook.local>
2017-10-02 21:34:43 +0530

    Initial Commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,12 @@
-#
-# Cookbook:: nginx
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+execute "update-upgrade" do 
+	command "apt-get update && apt-get upgrade -y"
+	action :run
+end
+
+package "apache2" do
+	action:install
+end
+
+service 'apache2' do
+	action [:enable, :start]
+end
